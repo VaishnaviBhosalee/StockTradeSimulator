16a4b482b2bbf658ec1c7aa41ad42298ff03fb79
Waffl3l0v3 <148672475+Waffl3l0v3@users.noreply.github.com>
2025-06-23 12:17:52 +0000

    models

--- models/user_finance.py
+++ models/user_finance.py
@@ -3,10 +3,10 @@ from extensions import db
 
 class Finance(db.Model):
     __tablename__='finances'
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id',ondelete='CASCADE'))
-    current_balance = db.Column(db.Integer,default=100000)
-    todays_change = db.Column(db.Integer)
+    current_balance = db.Column(db.Float,default=100000)
+    todays_change = db.Column(db.Float)
     
     def __repr__(self):
         return f'<Finance {self.id}>'
--- models/user_stocks.py
+++ models/user_stocks.py
@@ -3,14 +3,14 @@ from extensions import db
 
 class Stock(db.Model):
     __tablename__='stocks'
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    user_id=db.Column(db.Integer,db.ForeignKey('users.id',ondelete='CASCADE'))
-    stock_name=db.Column(db.String(50),unique=True)
-    buy_price_of_user=db.Column(db.Integer,nullable=False)
-    qty=db.Column(db.Integer,unique=True)
-    symbol=db.Column(db.String(50),unique=True)
-    total_value=db.Column(db.Float,unique=True)
-    Status=db.Column(db.String(50),unique=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id',ondelete='CASCADE'))
+    stock_name = db.Column(db.String(50), unique=True)
+    buy_price_of_user = db.Column(db.Integer, nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    stock_symbol = db.Column(db.String(50), unique=True)
+    total_value = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
 
     
     def __repr__(self):
