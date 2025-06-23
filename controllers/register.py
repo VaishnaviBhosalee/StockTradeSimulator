d82d95f55bb55114b8a5172011be3e3b97912017
Soham Kadam <sohamkadam0311@gmail.com>
2025-06-23 18:14:22 +0530

    pull out then go out

--- controllers/register.py
+++ controllers/register.py
@@ -1,7 +1,8 @@
 from flask import render_template,redirect
 from create_app import app
 from extensions import db
-
+from models.user import User
 @app.route('/register')
 def register():
+    
     return render_template('register.html')
