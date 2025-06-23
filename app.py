3b94414394569dbff0b3caf6ff4a3e2fcf5dcc62
Soham Kadam <sohamkadam0311@gmail.com>
2025-06-24 00:30:42 +0530

    always pull and push

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 from create_app import app
 from extensions import db
-from flask import render_template
+from flask import render_template,request,jsonify
 from dotenv import load_dotenv
 import os
 from controllers import *
-
+import requests
 # ---- SET DATABASE BINDS -----------------------------------------
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db.init_app(app)
+FINNHUB_KEY = os.getenv("FINNHUB_KEY")
 
 # ---- load secrets -------------------------------------------------
 load_dotenv()
 
-# ---- Import API route (executes @app.route) ----
-import controllers.api_search_symbol
-
 # ---- UI ROUTES ----------------------------------------------------
 @app.route('/')
 def landingPage():
+    
     return render_template('landing_page.html')
+
+
+@app.route("/api/quote")
+def quote():
+    symbol = request.args.get("symbol", "").upper()
+    res = requests.get("https://finnhub.io/api/v1/quote", params={
+        "symbol": symbol,
+        "token": FINNHUB_KEY
+    })
+    return jsonify(res.json())
+
+@app.route("/api/chart")
+def chart():
+    symbol = request.args.get("symbol", "").upper()
+    res = requests.get("https://finnhub.io/api/v1/stock/candle", params={
+        "symbol": symbol,
+        "resolution": "D",
+        "count": 7,
+        "token": FINNHUB_KEY
+    })
+    return jsonify(res.json())
 
 # ---- run the server ----------------------------------------------
 if __name__ == '__main__':
