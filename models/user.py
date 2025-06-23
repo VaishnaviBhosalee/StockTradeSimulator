9313cd343b69f435552b206653ddcdb68ca0c2a6
Waffl3l0v3 <148672475+Waffl3l0v3@users.noreply.github.com>
2025-06-23 11:58:02 +0000

    stashing

--- models/user.py
+++ models/user.py
@@ -1,16 +1,16 @@
 from extensions import db
-
+from datetime import datetime, timezone
 
 class User(db.Model):
     __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email=db.Column(db.String(50),unique=True)
-    password=db.Column(db.String(20),nullable=False)
-    fullname=db.Column(db.String(100),nullable=False)
-    joined_data=db.Column(db.Datetime,unique=True)
+    email = db.Column(db.String(50),unique=True, nullable=False)
+    password = db.Column(db.String(20),nullable=False)
+    fullname = db.Column(db.String(100),nullable=False)
+    joined_data = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     display_name = db.Column(db.String(20), unique=True, nullable=False)
-    user_finance=db.relationship('Finance',backref='u')
+    user_finance = db.relationship('Finance',backref='u')
     
     def __repr__(self):
         return f'<User {self.username}>'
