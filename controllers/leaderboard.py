141e6fd1a386543d8916d50ccc5085a1347cb0ea
Soham Kadam <sohamkadam0311@gmail.com>
2025-06-24 13:01:31 +0530

    pull

--- controllers/leaderboard.py
+++ controllers/leaderboard.py
@@ -1,7 +1,9 @@
 from flask import render_template,redirect
 from create_app import app
 from extensions import db
-
+from models.user import User
 @app.route('/leaderboard/<user_name>/<login_success>')
 def leaderboard(user_name,login_success):
-    return render_template('leaderboard.html', user_name=user_name,login_success=login_success)
+    users=User.query.all()
+    
+    return render_template('leaderboard.html', user_name=user_name,login_success=login_success,users=users)
