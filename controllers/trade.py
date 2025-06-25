8edcd917b70b1867ac77debbf3a179265274c523
Waffl3l0v3 <148672475+Waffl3l0v3@users.noreply.github.com>
2025-06-25 04:21:23 +0000

    gonna delete my changes

--- controllers/trade.py
+++ controllers/trade.py
@@ -2,7 +2,17 @@ from flask import render_template,redirect
 from create_app import app
 from extensions import db
 from datetime import datetime
-@app.route('/trade/<user_name>/<login_success>')
+from models.user_stocks import UserStocks
+
+
+@app.route('/trade/<user_name>/<login_success>', methods=['POST','GET'])
 def trade(user_name,login_success):
     current_time = datetime.now()
+    # if request.method == 'POST':
+    #     new_stock_name = request.form['stock_name']
+    #     new_stock_symbol = request.form['stock_symbol']
+    #     new_stock_total_price = request.form['total_price']
+    #     new_qty = request.form['qty']
+    #     new_stock_indi_price = 
+
     return render_template('trade.html', user_name=user_name,login_success=login_success,current_time=current_time)
